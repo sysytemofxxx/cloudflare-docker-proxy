30b3965b548b913584646d05ea844fe59faf7ad5
sysytemofxxx <957217410@qq.com>
2024-10-08 17:17:55 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.limonada.top": dockerHub,
+  "quay.limonada.top": "https://quay.io",
+  "gcr.limonada.top": "https://gcr.io",
+  "k8s-gcr.limonada.top": "https://k8s.gcr.io",
+  "k8s.limonada.top": "https://registry.k8s.io",
+  "ghcr.limonada.top": "https://ghcr.io",
+  "cloudsmith.limonada.top": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.lixd.xyz": dockerHub,
+  "docker-staging.limonada.top": dockerHub,
 };
 
 function routeByHosts(host) {
